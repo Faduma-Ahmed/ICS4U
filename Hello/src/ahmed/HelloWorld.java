523ce0551ffe8db6297ddb7aa4fdffb645bc11df
Faduma Ahmed <Ahmed.faduma-@hotmail.com>
2015-09-29 13:09:53 -0400

    Adding HelloWorld, Greetings, and Greetings2

---
+++ Hello/src/ahmed/HelloWorld.java
@@ -0,0 +1,18 @@
+package ahmed;
+
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Displays a 'Hello World" message on the screen
+		 */
+		
+		System.out.println("Hello World !");
+
+	}
+
+}
